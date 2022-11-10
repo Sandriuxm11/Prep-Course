db149563325cbdebe009ca16b27f5299ca67d375
Sandriuxm11 <samaya.dudas@gmail.com>
2022-11-10 14:05:52 -0500

    segunda version tarea

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -20,6 +20,12 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if(edad>=18){
+    return 'Allowed'
+  }
+  else(edad<18);{
+    return 'Not allowed'
+  }
 }
   
 function conection(status) {
@@ -28,6 +34,15 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if(status===1){
+    return 'Online'
+  }
+  else if(status===2){
+    return 'Away'
+  }
+  else{
+    return 'Offline'
+  }
 }
 
 function saludo(idioma) {
@@ -37,6 +52,21 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma='aleman'){
+    return 'Guten Tang!'
+  }
+  if(idioma='mandarin'){
+    return 'Ni Hao!'
+  }
+  if(idioma='ingles'){
+    return 'Hello!'
+  }
+  if(idioma !== 'aleman' || idioma !== 'mandarin' || idioma !== 'ingles'){
+    return 'Hola!'
+  }
+  else if(idioma=undefined){
+    return 'Hola!'
+  }
 }
 
 function colors(color) {
